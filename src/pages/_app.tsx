c09d9de1675d57a56288d18869fa9cbb42a613d0
sim1222 <kokt@sim1222.com>
2023-05-23 01:16:05 +0900

    fix font with use nuxt/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,17 @@ import '@/styles/reset.css'
 import '@/styles/global.scss'
 import '@/styles/variables.scss';
 import type { AppProps } from 'next/app'
+import { Zen_Kaku_Gothic_New } from "@next/font/google";
+
+const ZenKakuGothicNew = Zen_Kaku_Gothic_New({
+	weight: ["300", "400", "500", "700"],
+	display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={ZenKakuGothicNew.className}>
+			<Component {...pageProps} />
+		</main>
+)
 }
